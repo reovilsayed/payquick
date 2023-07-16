8a01445b92234ae79ae4cfbb3b12c36ef0b7c6d4
sayed khan <reovilsayed@gmail.com>
2023-07-16 09:07:14 +0600

    123

--- payquick.php
+++ payquick.php
@@ -254,10 +254,13 @@ function custom_quickpay_gateway_class()
 							$payment = new WC_QuickPay_API_Payment();
 
 							// Retrieve resource data about the transaction
-							    $payment->get( $transaction_id );
+							$payment->get( $transaction_id );
+
+							// Check if the transaction can be captured
+							if ( $payment->is_action_allowed( 'capture' ) ) {
 								$amount            = WC_QuickPay_Helper::price_multiplied_to_float( $order->get_total(), $payment->get_currency() );
 								$payment->capture( $transaction_id, $order, $amount );
-
+							}
 						} catch ( QuickPay_Capture_Exception $e ) {
 							woocommerce_quickpay_add_runtime_error_notice( $e->getMessage() );
 							$order->add_order_note( $e->getMessage() );
